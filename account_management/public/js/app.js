d9ee6aa8e3e473344eaa0694ea06ac373b957a92
anna <anna@wax.be>
2021-03-11 17:56:50 +0100

    nav

--- account_management/public/js/app.js
+++ account_management/public/js/app.js
@@ -19432,8 +19432,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/annadelanghe/Documents/extra time/account_management/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/annadelanghe/Documents/extra time/account_management/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/annadelanghe/Sites/Own/account_management/account_management/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/annadelanghe/Sites/Own/account_management/account_management/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
