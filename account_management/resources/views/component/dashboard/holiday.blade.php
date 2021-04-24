d7e3fc7eaeb071d25841f115f6227f5a1d77d727
JensDeMuynck <demuynck.jens@gmail.com>
2021-04-24 22:14:42 +0200

    [FEATURE] [DASHBOARD] Component holiday styling.

--- account_management/resources/views/component/dashboard/holiday.blade.php
+++ account_management/resources/views/component/dashboard/holiday.blade.php
@@ -1,3 +1,39 @@
 <div class="dashboard-holiday">
-    <h1>holiday</h1>
+    <div class="dashboard-holiday__day">
+        <h2 class="dashboard-holiday__day-name">VR</h2>
+
+        <div class="dashboard-holiday__shape dashboard-holiday__shape--first"></div>
+
+        <div class="dashboard-holiday__date">4 Dec '20</div>
+    </div>
+    <div class="dashboard-holiday__day">
+        <h2 class="dashboard-holiday__day-name">MA</h2>
+
+        <div class="dashboard-holiday__shape"></div>
+
+        <div class="dashboard-holiday__date">21 Dec '20</div>
+    </div>
+    <div class="dashboard-holiday__day dashboard-holiday__day--today">
+        <h2 class="dashboard-holiday__day-name">DO</h2>
+
+        <div class="dashboard-holiday__shape">
+            <div class="dashboard-holiday__today-type">Werkdag</div>
+        </div>
+
+        <div class="dashboard-holiday__date">24 Dec '20</div>
+    </div>
+    <div class="dashboard-holiday__day">
+        <h2 class="dashboard-holiday__day-name">VR</h2>
+
+        <div class="dashboard-holiday__shape"></div>
+
+        <div class="dashboard-holiday__date">8 Jan '21</div>
+    </div>
+    <div class="dashboard-holiday__day">
+        <h2 class="dashboard-holiday__day-name">DO</h2>
+
+        <div class="dashboard-holiday__shape dashboard-holiday__shape--last"></div>
+
+        <div class="dashboard-holiday__date">14 Oct '20</div>
+    </div>
 </div>
