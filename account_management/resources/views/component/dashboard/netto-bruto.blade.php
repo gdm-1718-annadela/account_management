e4bd29f12c87aaa70f7d586998454c824edee958
JensDeMuynck <demuynck.jens@gmail.com>
2021-04-24 21:32:11 +0200

    [FEATURE] [DASHBOARD] Component netto-bruto styling.

--- account_management/resources/views/component/dashboard/netto-bruto.blade.php
+++ account_management/resources/views/component/dashboard/netto-bruto.blade.php
@@ -1,3 +1,15 @@
 <div class="dashboard-netto-bruto">
-    <h1>netto-bruto</h1>
+    <h1 class="dashboard-netto-bruto__title">Payment Index</h1>
+    <div class="dashboard-netto-bruto__data">
+        <div class="dashboard-netto-bruto__netto">
+            <h2>
+                €<span>1.678</span>,39
+            </h2>
+        </div>
+        <div class="dashboard-netto-bruto__bruto">
+            <h2>
+                €<span>1.800</span>,00
+            </h2>
+        </div>
+    </div>
 </div>
