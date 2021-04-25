01ee4176f5f019100c517191b53d157e82bfaa21
anna <annadelanghe@hotmail.be>
2021-04-25 12:28:16 +0200

    [FEATURE] [ACCOUNTS] Setup frontend template.

--- account_management/resources/views/pages/accounts.blade.php
+++ account_management/resources/views/pages/accounts.blade.php
@@ -1,16 +1,34 @@
 @extends('layout')
 
-@section('content')
-    <h1>Accounts</h1>
+@section('content') 
+
+<div class="accounts__container">
     @foreach($accounts as $account)
-        <div>
-            {{$account->field_account_name}}
-            {{$account->field_account_desc}}
-            {{$account->field_account_total_amount}}
-            {{$account->updated_at}}
-            <a href="{{ route('viewAccount', $account->id) }}">view</a>
-            <a href="{{ route('editAccount', $account->id) }}">edit</a>
-            <a href="{{ route('deleteAccount', $account->id) }}">delete</a>
+        <div class="accounts__account">
+            <div class="accounts__profile-picture">
+                <div class="accounts__profile-name">Z</div>
+            </div>
+
+            <div class="accounts__details">
+                {{$account->field_account_name}}
+                {{$account->field_account_desc}}
+            </div>
+
+            <div class="accounts__amount">
+                {{$account->field_account_total_amount}}
+            </div>
+
+            <div class="accounts__date">
+                {{$account->updated_at}}
+            </div>
+
+            <div class="accounts__actions">
+                <a href="{{ route('viewAccount', $account->id) }}">view</a>
+                <a href="{{ route('editAccount', $account->id) }}">edit</a>
+                <a href="{{ route('deleteAccount', $account->id) }}">delete</a>
+            </div>
         </div>
     @endforeach
+</div>
+
 @endsection
